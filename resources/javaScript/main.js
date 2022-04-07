6867ad3ce80d7df6a9ba1f02e2075c048e2a9696
Cristian Guerrero <cristian1guerrero@hotmail.com>
2022-04-06 22:41:29 -0300

    master

--- resources/javaScript/main.js
+++ resources/javaScript/main.js
@@ -1,5 +1,5 @@
 const ESPACIO = (" ");
-
+const UVA = 18;
 
 let $banco = document.getElementById('banco');
 let bancos = ['Santander taza UVA', 'Santander taza ESPECIAL', 'Santander taza tradicional', 'IUDU MILA SUPERVILLER']
@@ -14,39 +14,23 @@ function mostrarBancos(arreglo, lugar) {
 
 mostrarBancos(bancos, $banco);
 
-$banco.addEventListener( function () {
-    let valor = $banco.value
-    switch (valor) {
-        case 'Santander taza UVA':
-            let UVA  = 18 / 100;
-            break
-
-        case 'Santander taza ESPECIAL':
-
-            let taza_especial = 30 / 100;
-            break
-
-        case 'Santander taza tradicional':
-            let taza_fija_santa = 55.5 / 100;
-            break
-
-        case 'IUDU MILA SUPERVILLER':
-            let taza_fija_mila = 52 / 100;
-            break
-    }
-
-})
 
 
-//santander uva
+function Cliente(plata,cuotas,bancos){
+    this.plata =plata;
+    this.cuotas=cuotas;
+    
+}
+
 function tazaDel18() {
     let plata = parseInt(document.getElementById("dinero_a_prestar").value);
     let cuotas = parseInt(document.getElementById("cantidad_de_cuotas").value);
     let cuotas_por_mes = plata / cuotas;
-    let interes = cuotas_por_mes * valor;
+    let interes = cuotas_por_mes * UVA;
     let final_por_mes_18 = cuotas_por_mes + interes;
-
-    if ((cuotas >= 1) && (cuotas <= 60) && (document.getElementById("santander_taza_UVA"))) {
+    const cliente2 = new Cliente(plata,cuotas);
+    console.log(cliente2)
+    if (cuotas >= 1 && cuotas <= 60){
         alert(`tu cuotas mensuales serian de ${ESPACIO}` + final_por_mes_18 + `En caso de ser cuota UVA, recuerde que las cuotas pueden aumentar segun la inflacion del pais`);
 
     }
